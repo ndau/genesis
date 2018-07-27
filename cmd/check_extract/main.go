f37781235b6613f08b3a8a3ccbd7d34ba8f02736
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2018-07-27 14:02:27 +0200

    Add cmd demonstrating successful extraction of spreadsheet data

---
+++ cmd/check_extract/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/oneiro-ndev/genesis/pkg/etl"
+)
+
+func check(err error) {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, fmt.Sprintf("%v", err))
+		os.Exit(1)
+	}
+}
+
+func getNdauhome() string {
+	nh := os.ExpandEnv("$NDAUHOME")
+	if len(nh) > 0 {
+		return nh
+	}
+	return filepath.Join(os.ExpandEnv("$HOME"), ".ndau")
+}
+
+func main() {
+	path := etl.DefaultConfigPath(getNdauhome())
+	var rows []etl.RawRow
+	var err error
+	err = etl.WithConfig(path, func(conf *etl.Config) error {
+		rows, err = etl.Extract(conf)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	check(err)
+	fmt.Println("Rows extracted:", len(rows))
+	fmt.Printf("First row: %v\n", rows[0])
+	if len(rows) > 0 {
+		fmt.Printf("Last row:  %v\n", rows[len(rows)-1])
+	}
+}
---
+++ pkg/etl/extract.go
@@ -0,0 +1,109 @@
+package etl
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/tealeg/xlsx"
+)
+
+// each field of a RawRow needs a const string identifier
+const (
+	AddressS           = "address"
+	QtyPurchasedS      = "qty_purchased"
+	PurchaseDateS      = "purchase_date"
+	UnlockDateS        = "unlock_date"
+	NotifyImmediatelyS = "notify_immediately"
+)
+
+// RawRow encapsulates the raw data of a single row of the ndau spreadsheet
+type RawRow struct {
+	Address      string
+	QtyPurchased float64
+	PurchaseDate time.Time
+	UnlockDate   *time.Time
+}
+
+type blank struct{}
+
+func (blank) Error() string {
+	return "Row is blank"
+}
+
+func isBlank(err error) bool {
+	_, ok := err.(*blank)
+	return ok
+}
+
+func extractRow(xrow *xlsx.Row, conf *Config, date1904 bool) (rr RawRow, err error) {
+	getCell := func(name string) *xlsx.Cell {
+		col := conf.Columns[name]
+		if col >= len(xrow.Cells) {
+			return nil
+		}
+		return xrow.Cells[col]
+	}
+
+	ac := getCell(AddressS)
+	if ac != nil {
+		rr.Address = ac.String()
+	}
+	qpc := getCell(QtyPurchasedS)
+	if qpc != nil {
+		rr.QtyPurchased, err = qpc.Float()
+		if err != nil {
+			return RawRow{}, err
+		}
+	}
+	pdc := getCell(PurchaseDateS)
+	if pdc != nil {
+		rr.PurchaseDate, err = pdc.GetTime(date1904)
+		if err != nil {
+			return RawRow{}, err
+		}
+	}
+	udc := getCell(UnlockDateS)
+	if udc != nil {
+		ud, err := udc.GetTime(date1904)
+		if err != nil {
+			return RawRow{}, err
+		}
+		if ud != xlsx.TimeFromExcelTime(0, date1904) {
+			rr.UnlockDate = &ud
+		}
+	}
+
+	return rr, nil
+}
+
+// Extract the input spreadsheet into a list of raw rows
+func Extract(conf *Config) ([]RawRow, error) {
+	err := conf.CheckColumns()
+	if err != nil {
+		return nil, err
+	}
+
+	file, err := xlsx.OpenFile(conf.Path)
+	if err != nil {
+		return nil, err
+	}
+	sheet, ok := file.Sheet[conf.Sheet]
+	if !ok {
+		return nil, fmt.Errorf("Sheet '%s' not found in %s", conf.Sheet, conf.Path)
+	}
+
+	raws := make([]RawRow, 0, sheet.MaxRow-conf.FirstRow+1)
+	for row := conf.FirstRow; row < sheet.MaxRow; row++ {
+		raw, err := extractRow(sheet.Rows[row], conf, file.Date1904)
+		if err != nil {
+			if isBlank(err) {
+				// if it's just a blank row, it's not a problem
+				continue
+			}
+			return nil, fmt.Errorf("Failure to extract row %d: %s", row, err.Error())
+		}
+		raws = append(raws, raw)
+	}
+
+	return raws, nil
+}
