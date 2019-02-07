f176dc556ee8b5da84b73af8f067c272bcbd2352
John Greenland <jsgjnr@yahoo.com>
2019-02-07 15:25:03 -0500

    clean up comments and imports

--- bin/process_csv.py
+++ bin/process_csv.py
@@ -4,10 +4,7 @@
 Process csv for ETL script
 """
 
-import os # for environment variables, path, and exits
-import subprocess # for running commands
 import sys # to print to stderr
-import functools # for lru_cache on preflight calls
 import csv
 import pymongo
 from pymongo import MongoClient
@@ -15,31 +12,19 @@ import pdb
 
 """ 
 # if getting timeout from MongoDB, you might have to whitelist your IP for access:
-# login to AWS, 
-# 
-# go to EC2
-
-N Virginia east
-
-click on the 14 instances
-
-click on the one starting with Primary
-
-Click on the security groups below
-
-click anyone of them
-
-there will be a filter put in the above search
-
-take it out
-
-and put Mongo in there
-
-you should see 3 security groups
-
-click on each one of those and look at the Inbound tab below
-
-that is where you add the firewall rule
+- login to AWS, 
+- go to EC2
+- N Virginia east
+- click on the 14 instances
+- click on the one starting with Primary
+- Click on the security groups below
+- click anyone of them
+- there will be a filter put in the above search
+- take it out
+- and put Mongo in there
+- you should see 3 security groups
+- click on each one of those and look at the Inbound tab below
+- that is where you add the firewall rule
 """
 
 def main():
@@ -64,7 +49,7 @@ def main():
         for p in sys.path:
             print(p)
 
-
+    # list of node addresses to be assigned to accounts
     node_list = ['ndarw5i7rmqtqstw4mtnchmfvxnrq4k3e2ytsyvsc7nxt2y7',
         'ndaq3nqhez3vvxn8rx4m6s6n3kv7k9js8i3xw8hqnwvi2ete',
         'ndahnsxr8zh7r6u685ka865wz77wb78xcn45rgskpeyiwuza',
@@ -73,6 +58,7 @@ def main():
 
     node_list_index = 0
 
+    # connect to MongoDB
     client = MongoClient('mongodb://admin:0n13r0Nd3v@34.228.30.229:27017')
     if verboseFlag:
         print(f'client = {client}')
@@ -92,25 +78,32 @@ def main():
     lines = list(r)
     if verboseFlag:
         print(f"addresses = {first['addresses']}")
+    # loop through MongoDB records
     for record in collection.find():
         if verboseFlag:
             print(f'record = {record}')
         addr_index = 0
         addrs = record['addresses']
+        # loop through rows in spreadsheet
         for line in lines:
+            # if userID in Mongo = userID in spreadsheet row, insert address into spreadsheet row
             if record['userId'] == line[8]:
                 if addr_index == 0:
                     first_line = line
 #                pdb.set_trace()
+                # if we don't have enough addresses in Mongo for this userID, print a message
                 if addr_index >= len(addrs):
                     print(f'addr mismatch, num in Mongo: {len(addrs)}, num in CSV: {addr_index}')
                 else:
                     line[3] = addrs[addr_index]
                 addr_index += 1
             # pdb.set_trace()
+            # if this row has data in it, assign a node address to this account in round robin
             if line[13] != '':
                 line[12] = node_list[node_list_index]
                 node_list_index = (node_list_index + 1) % len(node_list)
+        # if we have too many addresses in Mongo for this userID, print a message.  This happens
+        # on occasion because early on we generated too many addresses for each userID
         if addr_index != len(addrs):
             print(f'addr mismatch, num in Mongo: {len(addrs)}, num in CSV: {addr_index}')
             first_line[11] = f'Mongo: {len(addrs)}'
